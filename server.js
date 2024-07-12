6112eb583f57a35d19f0b48b8cacd3faee4a5d62
Richard <richardardilaz17@hotmail.com>
2024-07-12 11:13:55 -0400

    created configuration for check boxes, listen changes for checkbox and server

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
